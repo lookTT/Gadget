f187fb1a38479387b2b775f051c1c7d6a2dc0530
lookTT <zltdhr@gmail.com>
2018-10-31 23:35:39 +0800

    fix

--- formatWXMP.js
+++ formatWXMP.js
@@ -4,12 +4,13 @@
 var fs = require("fs");
 
 fs.readFile('in.txt', 'utf8', function (err, data) {
-    data = data.replace(/\r\n \r\n\r\n/g, "\r\n");
-    data = data.replace(/\r\n\r\n\r\n/g, "\r\n");
     data = data.replace(/ \r\n/g, "\r\n");
+    data = data.replace(/\r\n\r\n\r\n/g, "\r\n");
+
     
     data = data.replace(/\r\n#/g, "\r\n\r\n#");
     data = data.replace(/\r\n留言/g, "\r\n\r\n留言");
+    data = data.replace(/>/g, " > ");
 
     fs.writeFile('out.txt', data, function (err) {
         if (err) {
