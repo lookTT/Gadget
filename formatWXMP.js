ea0e1f3bb11d76dcd8d73051ef391008ebb64e31
lookTT <zltdhr@gmail.com>
2018-12-03 15:12:14 +0800

    更新一点逻辑
    更新一点逻辑

--- formatWXMP.js
+++ formatWXMP.js
@@ -4,14 +4,18 @@
 var fs = require("fs");
 
 fs.readFile('in.txt', 'utf8', function (err, data) {
-    data = data.replace(/ \r\n/g, "\r\n");
-    data = data.replace(/\r\n /g, "\r\n");
+    // data = data.replace(/ \r\n/g, "\r\n");
+    // data = data.replace(/\r\n /g, "\r\n");
+    data = data.replace(/ /g, "");
     data = data.replace(/\r\n\r\n\r\n/g, "\r\n");
 
 
     data = data.replace(/\r\n#/g, "\r\n\r\n#");
     data = data.replace(/\r\n留言/g, "\r\n\r\n留言");
+
+    data = data.replace(/ > /g, ">");
     data = data.replace(/>/g, " > ");
+    data = data.replace(/-/g, "- ");
 
     fs.writeFile('out.txt', data, function (err) {
         if (err) {
